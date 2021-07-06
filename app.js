4d290810a977cb8d81aaf1e7b2bf04b875162dd7
Vikas Baghel <vikasbaghel26@gmail.com>
2021-07-06 17:19:25 +0530

    Custom notFound route added

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
-
+const notFound = require('./middleware/not-found')
 const port = 3000
 
+// middleware
 // static routes
 app.use(express.static('./public'))
 
@@ -15,6 +16,8 @@ app.use(express.json())
 
 //routes
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 const start =  async () => {
     try {
